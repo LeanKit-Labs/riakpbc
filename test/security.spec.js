a73cc5937e59b5e67f1dd65f52d4b1561250d5d8
Brian Edgerton <brian.edgerton@leankit.com>
2015-06-23 10:48:20 -0400

    Refactored tests to work with gulp and mocha.

---
+++ test/security.spec.js
@@ -0,0 +1,54 @@
+require( './setup.js' );
+var RiakPBC = require( '../' );
+var Exec = require( 'child_process' ).exec;
+
+describe( 'Security', function() {
+
+	describe( '(disabled)', function() {
+
+		it( 'returns an error when attempting to use auth without a secure node', function( done ) {
+
+			var client = RiakPBC.createClient( { auth: { user: 'riak', password: 'testing' } } );
+			client.ping( function( err ) {
+
+				expect( err ).to.exist;
+				expect( err.message ).to.equal( 'Security not enabled; STARTTLS not allowed.' );
+				done();
+			} );
+		} );
+	} );
+
+	describe( '(enabled)', function() {
+
+		before( function( done ) {
+
+			Exec( 'make enable-security', { env: process.env }, done );
+		} );
+
+		it( 'returns an error when attempting to use a secure node without auth', function( done ) {
+
+			var client = RiakPBC.createClient();
+			client.ping( function( err ) {
+
+				expect( err ).to.exist;
+				expect( err.message ).to.equal( 'Security is enabled, please STARTTLS first' );
+				done();
+			} );
+		} );
+
+		it( 'automatically upgrades to a secure connection when auth is provided', function( done ) {
+
+			var client = RiakPBC.createClient( { auth: { user: 'riak', password: 'testing' } } );
+			client.ping( function( err ) {
+
+				expect( err ).to.not.exist;
+				done();
+			} );
+		} );
+
+		after( function( done ) {
+
+			Exec( 'make disable-security', { env: process.env }, done );
+		} );
+	} );
+} );
